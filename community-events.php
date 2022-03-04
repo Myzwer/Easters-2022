ec382a3672d0096e5f0aafdbfc50a5c6fe5c88d2
Myzwer <josh@onefortyfivedesign.com>
2022-03-04 14:07:11 -0500

    :tada: Initial Commit

---
+++ community-events.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Community Events
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage easters-2022
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+<?php
+get_footer();
---
+++ experience.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Experience
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage easters-2022
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+<?php
+get_footer();
