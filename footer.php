66dbf840e3ff6858fbbea0d306ec7a3c0c9ac2bc
Myzwer <josh@onefortyfivedesign.com>
2022-03-05 01:16:49 -0500

    :art: Fixed the footer based on the mockup design.

--- footer.php
+++ footer.php
@@ -16,39 +16,15 @@
 
 <!--Start Footer-->
 <footer class="footer">
-    <div class="footer-left">
 
-        <!--Company Logo-->
-        <img src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png">
-        <div class="left-inner">
-            <!--Company Info-->
-            <h3>Fancy Company</h3>
-            <p class="phone-number">+1 234-567-8901</p>
-        </div>
-    </div>
 
-    <div class="footer-right">
-        <div class="right-inner">
-            <!--Wordpress Generated Link Lists-->
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-1' ) ); ?>
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-2' ) ); ?>
-        </div>
-
-    </div>
     <div class="footer-bottom">
         <div class="bot-inner">
 
             <!--Copyright Info-->
-            <p>Some Text</p>
-            <p>© <?php echo date("Y"); ?> Website Company Name</p>
+            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-1' ) ); ?>
+            <p>© <?php echo date("Y"); ?> Foothills Church</p>
 
-            <!--Socials, icons from: https://fontawesome.com/ -->
-            <div class="footer-icons">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-            <a href="#"><i class="fa fa-linkedin"></i></a>
-            <a href="#"><i class="fa fa-youtube"></i></a>
         </div>
     </div>
 </footer>
