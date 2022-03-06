97e399501984e652186569f1c1a0d19a9fe880b0
Myzwer <josh@onefortyfivedesign.com>
2022-03-05 23:05:04 -0500

    :rocket: Finished the layout of the homepage.

--- frontpage.php
+++ frontpage.php
@@ -55,122 +55,141 @@ get_header(); ?>
     </div>
     <!--End Modal-->
 
-    <video class="header-video" src="https://foothillscollective.com/wp-content/uploads/2021/04/Res-Power-Background.mp4" autoplay loop playsinline muted></video>
+    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+  rgba(0, 0, 0, 0.45),
+  rgba(0, 0, 0, 0.45)
+), url('http://easters-2022.local/wp-content/uploads/2022/03/Background-Header.jpg') center center;
+ height: 60vh;">
+        <div class="content-middle text-white text-center">
+            <img class="content-middle w-11/12 md:w-4/12"
+                 src="http://easters-2022.local/wp-content/uploads/2022/03/Easters-Logo.png" alt="">
+        </div>
 
-    <div class="viewport-header">
-        <div class="head-container">
-            <div class="center add-padding">
-                <h1 class="text-white text-5xl pb-5">Header Title</h1>
+        <img class="hidden lg:block absolute right-0 bottom-0 w-3/12"
+             src="http://easters-2022.local/wp-content/uploads/2022/03/Flower-Corner.png" alt="">
+        <img class="hidden lg:block absolute left-0 bottom-0 w-3/12"
+             src="http://easters-2022.local/wp-content/uploads/2022/03/Tomb-Corner.png" alt="">
+    </div>
+
+    <div class="bg-black">
+        <div class="px-4 py-10">
+            <div class="grid grid-cols-12">
+                <div class="col-span-12">
+                    <div class="md:text-center">
+                        <h1 class="text-white text-4xl uppercase">Are you ready to experience <span class="text-yellow">life change?</span>
+                        </h1>
+                    </div>
+                </div>
             </div>
-            <hr class="text-white pb-5">
-            <h2 class="text-white text-3xl ">Title</h2>
-            <h3 class="text-white text-2xl">Subtitle</h3>
         </div>
     </div>
 
     <div class="bg-white pb-10">
-        <div class="m-4 md:m-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
-            <div class="grid">
-                <div class=" grid-cols-12">
-                    <div class="col-span-12">
-                        <div class="text-center md:text-left mb-1">
-                            <h1>Color Pod</h1>
-                            <p>All fields optional</p>
+        <div class="lg:max-w-4xl lg:mx-auto pt-10">
+
+            <!-- Outer -->
+            <div class="grid grid-cols-12 gap-4 px-5">
+                <div class="col-span-12 md:col-span-8">
+                    <!-- Inner -->
+                    <div class="grid grid-cols-12 gap-4">
+                        <div class="col-span-12">
+                            <div class="col-span-12">
+                                <div class="text-center text-black uppercase font-bold text-md">Maryville Location</div>
+                            </div>
                         </div>
-                        <hr>
-                    </div>
-                </div>
-            </div>
-
-            <div class="grid grid-cols-12 gap-4 mt-6">
-                <div class="col-span-12 lg:col-span-4 card-gradient-1 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-2 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-3 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="grid grid-cols-12 mt-20">
-                <div class="col-span-12">
-                    <div class="text-center md:text-left mb-1">
-                        <h1>PDF Download (four-up)</h1>
-                        <p>This configuration always shows four PDF's on one row.</p>
-                    </div>
-                    <hr>
-                </div>
-            </div>
-
-
-            <div class="grid grid-cols-12 mt-5 gap-6">
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
+                        <div class="col-span-12">
+                            <div class="bg-pink p-5 rounded-xl shadow-xl text-white">
+                                <h2 class="uppercase text-3xl">Saturday, April 26</h2>
+                                <h3 class="uppercase text-md">5pm Service</h3>
+                                <h3 class="uppercase text-md">1551 W. Lamar Alexander Pkwy <br/> Maryville TN 37801</h3>
+                                <button id="two"
+                                        class="button mx-auto lg:mx-0 bg-white text-black rounded-full my-2 py-2 px-5 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out z-5">
+                                    Mark Your Calendar
+                                </button>
+                            </div>
                         </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
+
+                        <div class="col-span-12">
+                            <div class="bg-pink p-5 rounded-xl shadow-xl text-white">
+                                <h2 class="uppercase text-3xl">Saturday, April 26</h2>
+                                <h3 class="uppercase text-md">5pm Service</h3>
+                                <h3 class="uppercase text-md">1551 W. Lamar Alexander Pkwy <br/> Maryville TN 37801</h3>
+                                <button id="two"
+                                        class="button mx-auto lg:mx-0 bg-white text-black rounded-full my-2 py-2 px-5 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out z-5">
+                                    Mark Your Calendar
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
+                <div class="col-span-12 md:col-span-4">
+                    <div class="grid grid-cols-12 gap-4">
+                        <div class="col-span-12">
+                            <div class="text-center text-black uppercase font-bold text-md">Maryville Location</div>
                         </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
+                        <div class="col-span-12">
+                            <div class="bg-green p-5 rounded-xl shadow-xl text-white homepage-height-lock">
+                                <h2 class="uppercase text-3xl">Saturday, April 26</h2>
+                                <h3 class="uppercase text-md">5pm Service</h3>
+                                <h3 class="uppercase text-md">1551 W. Lamar Alexander Pkwy <br/> Maryville TN 37801</h3>
+                                <button id="two"
+                                        class="button mx-auto lg:mx-0 bg-white text-black rounded-full my-2 py-2 px-5 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out z-5">
+                                    Mark Your Calendar
+                                </button>
+                            </div>
                         </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
-                        </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
+    <div class="bg-white pb-10">
+        <div class="lg:max-w-4xl lg:mx-auto pt-10">
+            <div class="grid grid-cols-12 gap-4 px-5 text-black">
+                <div class="col-span-12 md:col-span-12 py-8">
+                    <h2 class = "text-6xl">plan your experience</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta doloribus eius fugit harum magnam, odit pariatur porro provident quam recusandae repellat tempora totam. Consectetur fuga non nulla sit tempora.</p>
+                </div>
+                <div class="col-span-12 md:col-span-12 py-8">
+                    <h2 class = "text-6xl">sign up to be baptized</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta doloribus eius fugit harum magnam, odit pariatur porro provident quam recusandae repellat tempora totam. Consectetur fuga non nulla sit tempora.</p>
+                    <button
+                            class="button mx-auto lg:mx-0 border-2 border-black text-black rounded-full my-2 py-2 px-5 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out z-5">
+                        I want to be baptized
+                    </button>
+                </div>
+
+                <div class="col-span-12 md:col-span-12 py-8">
+                    <h2 class = "text-6xl">join us for a community event</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta doloribus eius fugit harum magnam, odit pariatur porro provident quam recusandae repellat tempora totam. Consectetur fuga non nulla sit tempora.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-green pb-10">
+        <div class="lg:max-w-4xl lg:mx-auto pt-10">
+            <div class="grid grid-cols-12 gap-4 px-5 text-white">
+                <div class="col-span-12 md:col-span-6">
+                    <h3 class="text-2xl uppercase">Who We Are</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aspernatur aut corporis dolorem, ea eaque eius eveniet iusto nam, nihil quas, unde voluptatum. Commodi doloremque, ipsa modi saepe sit voluptas.</p>
+                </div>
+
+                <div class="col-span-12 md:col-span-6">
+                    <?php if (have_posts()) : while (have_posts()) : the_post();
+                        the_content();
+                    endwhile;
+                    else: ?>
+                        <p>Sorry, no posts matched your criteria.</p>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
 <?php
