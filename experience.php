c97ff1113e504c744c4e4cf14646be3e8aec528d
Myzwer <josh@onefortyfivedesign.com>
2022-03-09 17:12:22 -0500

    :rocket: Finished the header on the experience page... I think. Might need tweaking as I check different resolutions.

--- experience.php
+++ experience.php
@@ -13,5 +13,60 @@
 
 get_header(); ?>
 
+    <!-- The Modal -->
+    <div id="modal-container">
+        <div class="modal-background">
+            <div class="modal">
+                <div class="modal-content">
+                    <div class="modal-top bg-no-repeat bg-scroll bg-cover relative"
+                         style="background: url('<?php the_field('modal_background'); ?>') center center;">
+
+                        <div class="modal-icon">
+                            <img src="<?php the_field('modal_icon'); ?>" alt="Foothills Church Icon">
+                        </div>
+                    </div>
+                    <div class="modal-inner">
+                        <p class="pb-5"><?php the_field('modal_text'); ?></p>
+
+                        <?php if (have_rows('main_button')): ?>
+                            <?php while (have_rows('main_button')): the_row(); ?>
+                                <a download
+                                   href="<?php the_sub_field('button_download'); ?>">
+                                    <button class="mx-auto lg:mx-0 w-full bg-green text-black font-bold rounded-full my-1 md:my-1 py-4 px-5 md:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                        <?php the_sub_field('button_text'); ?>
+                                    </button>
+                                </a>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+
+                        <?php if (have_rows('secondary_button')): ?>
+                            <?php while (have_rows('secondary_button')): the_row(); ?>
+                                <a href="<?php the_sub_field('button_link'); ?>">
+                                    <button class="mx-auto lg:mx-0 w-full border-black border-2 text-black font-bold rounded-full my-1 md:my-1 py-3 px-6 md:px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                        <?php the_sub_field('button_text'); ?>
+                                    </button>
+                                </a>
+                            <?php endwhile; ?>
+                        <?php endif;  ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End Modal-->
+
+
+    <div class="bg-green relative">
+        <div class="hidden lg:block absolute bottom-0 right-0 w-4/12">
+            <img src="https://easters-2022.local/wp-content/uploads/2022/03/Flower-Corner-1.png" alt="">
+        </div>
+        <div class="lg:mx-auto">
+            <div class="grid grid-cols-12 gap-4 text-black">
+                <div class="col-span-12 lg:col-span-9 px-5 py-20 md:p-10 md:my-24">
+                    <h1 class="uppercase leading-relaxed text-5xl md:text-8xl xl:text-9xl text-white">Are you ready to experience life change?</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 get_footer();
